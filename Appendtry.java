252be406382f8927e1c66e6bb2cd5140a46cb697
Shruti  Bhapkar <58063504+ShrutiBhapkar@users.noreply.github.com>
2022-10-30 07:40:20 -0400

    Add files via upload
    To Add a node in existing xml file using java

---
+++ Appendtry.java
@@ -0,0 +1,84 @@
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+
+public class Appendtry {
+
+public static void main(String[] args)
+{
+
+//
+Path root=Paths.get("C:\\Users\\smohanra\\Documents\\zip_trial\\zip_assembly_archive_2\\site.xml");
+String location=root.toString();
+subfoldercount(location);
+appendxml(location);
+}
+
+
+	public static void appendxml(String location) 
+	{
+try {
+File xmlFile = new File(location);
+String[] files = xmlFile.list();
+int  limit=subfoldercount(location)+1;
+//limit=limi;
+String Length=Integer.toString(limit);
+String Fileentry="product_tibco_tbs_product_p2_repo_4.19.0.303.zip";
+DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+Document document = documentBuilder.parse(xmlFile);
+Element documentElement = document.getDocumentElement();
+Element textNode = document.createElement("File");
+textNode.setAttribute("id", String.valueOf(Length));
+Element textNode1 = document.createElement("name");
+textNode1.setTextContent(Fileentry);
+Element nodeElement = document.createElement("File");
+nodeElement.appendChild(textNode);
+nodeElement.appendChild(textNode1);
+documentElement.appendChild(nodeElement);
+document.replaceChild(documentElement, documentElement);
+Transformer tFormer =
+TransformerFactory.newInstance().newTransformer();
+tFormer.setOutputProperty(OutputKeys.METHOD, "xml");
+Source source = new DOMSource(document);
+Result result = new StreamResult(xmlFile);
+tFormer.transform(source, result);
+} catch (Exception ex) {
+System.out.println(ex);
+}
+	}
+
+	 public static int subfoldercount(String location)
+	 {
+	 	 String path = location;
+	       DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	       int length = 0;
+	       try (InputStream is = new FileInputStream(path)) {
+
+	           DocumentBuilder db = dbf.newDocumentBuilder();
+
+	           Document doc = db.parse(is);
+
+	           // get all elements known as "staff"
+	           NodeList list = doc.getElementsByTagName("name");
+	           length = list.getLength();
+	           length= length-1;
+	            //1=subfolderxml 2nd for main archive
+	           System.out.println("Number of subfolders are : " + length);
+
+	       } catch (ParserConfigurationException | SAXException | IOException e) {
+	           e.printStackTrace();
+	       }
+	       
+	       return length;
+	 }
+}
